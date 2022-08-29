894a345ecdd6624e8043d3cb4b3d5301b5df168a
DOGHOUSETV <arubigeorge0@gmail.com>
2022-08-29 15:51:46 -0400

    Clean up code

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,8 +16,8 @@ const Home: NextPage = () => {
       <main className="h-screen flex items-center p-24">
         <div className="box-content bg-white h-full w-full p-8 overflow-hidden rounded-3xl">
           <div className="flex flex-row max-w-7xl mx-auto">
-            <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-              <div className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+            <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-2/3 lg:pb-28 xl:pb-32">
+              <div className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-4 xl:mt-20">
                 <div className="sm:text-center lg:text-left">
                   <h1 className="text-4xl tracking-tight font-bold text-gray-900 sm:text-5xl md:text-6xl">
                     <span className="mr-3">
@@ -27,8 +27,8 @@ const Home: NextPage = () => {
                       Radar Event Map</span>
                   </h1>
                   <p className="my-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                    Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-                    fugiat veniam occaecat fugiat aliqua.
+                  However, some great things do not leave the soul. He who is eager to take advantage of the Internet. 
+                  There are many people who want to run away from the dark.
                   </p>
                   <SearchBox />
                 </div>
