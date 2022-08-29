ea80efe00ff3e8dee1281657f1df014d6b522fda
DOGHOUSETV <arubigeorge0@gmail.com>
2022-08-29 15:50:17 -0400

    Call event endpoint

--- components/SearchBox.tsx
+++ components/SearchBox.tsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import Radar from 'radar-sdk-js';
+import axios from 'axios';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import radarCategories from '../miscellaneous/radar_places.json'
 
 const SearchBox = () => {
+  let publishableKey = process.env.NEXT_PUBLIC_RADAR_KEY
   const [category, setCategory] = useState('');
   const [slugs, setSlugs] = useState<any>([]);
-  const [location, setLocation] = useState<any>();
-  const [event, setEvent] = useState<any>();
+  const [userEvent, setUserEvent] = useState<any>([]);
 
   const handleChange = (e: any, value: any) => {
     e.preventDefault()
@@ -17,35 +18,23 @@ const SearchBox = () => {
 
   const handleClick = () => {
     const index = radarCategories.map(object => object.name).indexOf(category);
-    setSlugs(radarCategories[index].slugs);
-    getUserLocation()  
-  }
-  //console.log(slugs)
-
-  const getUserLocation = () => {
-    Radar.getLocation(async function(err: any, result: any) {
-      if (!err) {
-        // do something with result.location, result.events, result.user
-        const currentLocation = await result.location
-        setLocation(currentLocation)
-      }
-    });
-    return location;
+    setSlugs(radarCategories[index].slugs);  
   }
 
-  const getCategoryEvent = () => {
-    Radar.trackOnce({
-      latitude: location.latitude,
-      longitude: location.longitude,
-      accuracy: location.accuracy,
-    }, async function(err: any, result: any) {
-      if (!err) {
-        // do something with result.places
-        setEvent(await result.event)
-      }
-    });
+  const url = `https://api.radar.io/v1/events?placeCategories=${slugs}`;
+  useEffect(() => {
+    Radar.initialize(publishableKey);
+    getUserEvent();
+  });
+
+  const getUserEvent = async () => {
+    const { data } = await axios.get(`${url}`)
+    if(!category) {
+      return null
+    } else {
+      setUserEvent(data)
+    }
   }
-  //getCategoryEvent()
     
   return (
     <div className="z-10">
@@ -54,7 +43,7 @@ const SearchBox = () => {
           freeSolo
           id="free-solo-2-demo"
           disableClearable
-          sx={{ width: 350 }}
+          sx={{ width: 400 }}
           options={radarCategories.map((option: any) => option.name)}
           onChange={handleChange}
           renderInput={(params) => (
@@ -68,19 +57,11 @@ const SearchBox = () => {
             />
           )}
         />
-        <Autocomplete
-          id="free-solo-demo"
-          freeSolo
-          sx={{ width: 450 }}
-          options={["Use Current Location"]}
-          renderInput={(params) => <TextField {...params} label="Location" />}
-        />
       </div>
-      
       <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
         <div className="rounded-md shadow">
           <button className="w-full flex items-center justify-center px-8 py-3 border border-transparent 
-            text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg md:px-10"
+            text-base font-medium rounded-md text-white bg-[#007aff] hover:bg-blue-700 md:py-4 md:text-lg md:px-10"
             onClick={handleClick}
           >
             Go
@@ -89,7 +70,7 @@ const SearchBox = () => {
         <div className="mt-3 sm:mt-0 sm:ml-3">
           <a
             href="https://radar.com/documentation/places/categories"
-            className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-blue-700 bg-blue-100 hover:bg-blue-200 md:py-4 md:text-lg md:px-10"
+            className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-[#007aff] bg-blue-100 hover:bg-blue-200 md:py-4 md:text-lg md:px-10"
           >
             View Categories
           </a>
