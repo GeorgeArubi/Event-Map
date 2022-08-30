a9df4403417857129c629ee0e3de816747c15a18
DOGHOUSETV <arubigeorge0@gmail.com>
2022-08-30 13:49:26 -0400

    Render event data on map

--- components/EventMap.tsx
+++ components/EventMap.tsx
@@ -1,43 +1,26 @@
 import React, { useState } from 'react';
-import Radar from 'radar-sdk-js';
 import DeckGL from "@deck.gl/react/typed";
 import { IconLayer } from "@deck.gl/layers/typed";
 import { Map } from "react-map-gl";
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-const EventMap = (data: any) => {
-  const [location, setLocation] = useState<any>();
-  const [viewState, setViewState] = useState({
-      latitude: 38.889248,
-      longitude: -77.050636,
-      zoom: 12.5,
+const EventMap = (props: any, data: any) => {
+  const [viewState, setViewState] = useState<any>({
+      latitude: 40.05753979678667,
+      longitude: -76.44497274763935,
+      zoom: 5.1,
   })
+  
+  // Data to be used by the IconLayer
+  const ICON_MAPPING = {
+    marker: {x: 0, y: 0, width: 128, height: 128, mask: true}
+  };
 
-  const getUserLocation = () => {
-    Radar.getLocation(async function(err: any, result: any) {
-      if (!err) {
-        // do something with result.location, result.events, result.user
-        const currentLocation = await result.location
-        setLocation(currentLocation)
-      }
-    });
-  }
-  getUserLocation();  
-
-// Data to be used by the LineLayer
-const ICON_MAPPING = {
-  marker: {x: 0, y: 0, width: 128, height: 128, mask: true}
-};
-
-  data = [
-    {
-      name: 'Colma (COLM)', 
-      address: '365 D Street, Colma CA 94014', 
-      exits: 4214, 
-      coordinates: [-122.466233, 37.684638]
-    }
-  ]
-
+  data = props.categories;
+  //console.log(data)
+  
   const layer = new IconLayer({
     id: 'icon-layer',
     data,
@@ -46,23 +29,25 @@ const ICON_MAPPING = {
     // getIcon: return a string
     iconAtlas: 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/icon-atlas.png',
     iconMapping: ICON_MAPPING,
-    getIcon: d => 'marker',
-
+    getIcon: _d => 'marker',
     sizeScale: 9,
-    getPosition: d => d.coordinates,
-    getSize: d => 5,
+    getPosition: d => d.place.location.coordinates,
+    getSize: _d => 5,
     getColor: d => [Math.sqrt(d.exits), 140, 0]
   });
 
+  //console.log(viewState)
+
   return (
-    <div className="box-content w-3/4 z-10">
+    <div className="box-content w-2/3 z-10">
       <div className="relative z-10 pb-8 h-full sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
         <div className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-20 ">
             <DeckGL
               viewState={viewState}
+              onViewStateChange={e => setViewState(e.viewState)}
               controller={true}
               layers={[layer]}
-              getTooltip={({object}) => object && `${object.name}\n${object.address}`}
+              getTooltip={({object}) => object && `${object.place.name}\n`}
             >
                 <Map
                   mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
